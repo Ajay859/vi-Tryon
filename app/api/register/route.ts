cb9016260946e377ed683ef1dac42f50d29e46f9
Ajay Singh <koshyariajay7@gmail.com>
2026-03-22 14:53:32 +0530

    added auth.js authentication

---
+++ app/api/register/route.ts
@@ -0,0 +1,69 @@
+import prisma from "@/lib/prisma";
+import { signupSchema } from "@/lib/validation/auth";
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const validateData = signupSchema.parse(body);
+
+    const { name, email, password } = validateData;
+
+    const userExist = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (userExist) {
+      return NextResponse.json(
+        {
+          error: "user already exist plz login to continue",
+        },
+        {
+          status: 409,
+        },
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+    return NextResponse.json(
+      { message: "user created successfully", userId: user.id },
+      {
+        status: 201,
+      },
+    );
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      const formattedErrors = error.issues.map((issue) => ({
+        field: issue.path.join("."),
+        message: issue.message,
+      }));
+      return NextResponse.json(
+        {
+          error: "validation error",
+          details: formattedErrors,
+        },
+        {
+          status: 400,
+        },
+      );
+    }
+    console.error("Signup error", error);
+    return NextResponse.json(
+      {
+        message: "internal server error",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
---
+++ lib/validation/auth.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+export const signupSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(2, "name should be atleast 2 characters long")
+    .max(30, "name should be under 30 letters"),
+
+  email: z.email(),
+
+  password: z
+    .string()
+    .min(8, "password must be at least 8 characters long ")
+    .regex(/[a-z]/, {
+      message: "password must include at least 1 small letter",
+    })
+    .regex(/[A-Z]/, {
+      message: "password must include at least 1 capital letter",
+    })
+    .regex(/[0-9]/, { message: "password must include at least 1 number" })
+    .regex(/[^a-zA-Z0-9]/, {
+      message:
+        "password must include at least 1 special character (@$!%*?& etc.)",
+    }),
+});
+
+export type SignupInput = z.infer<typeof signupSchema>;
